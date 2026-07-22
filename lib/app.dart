2a73e262d1c3b5017a65dfe10192b08dffb44380
DevMohamad <mohamadnikandam1998@gmail.com>
2026-07-22 19:36:48 +0330

    feat: create ServicarApp widget for the main application structure

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ServicarApp extends StatelessWidget {
+  const ServicarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('Servicar'))),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:servicar/app.dart';
+
 void main() {
   runApp(const ServicarApp());
-}
-
-class ServicarApp extends StatelessWidget {
-  const ServicarApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Servicar'))),
-    );
-  }
 }
